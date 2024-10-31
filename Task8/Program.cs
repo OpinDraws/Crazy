ce3c74c916908db423b5032300861c996373482f
OpinDraws <blohin.m.m@gmail.com>
2024-10-31 23:26:12 +0300

    Add files via upload

---
+++ Task8/Program.cs
@@ -0,0 +1,42 @@
+﻿using MyVector;
+internal class Program
+{
+
+    static void Main()
+    {
+        MyStack<int> stack = new MyStack<int>();
+    }
+    public class MyStack<T> : MyVector<T>
+    {
+        MyVector<T> stack = new MyVector<T>();
+
+        public void Push(T item)
+        {
+            stack.Add(item);
+
+        }
+        public T Pop()
+        {
+            int top = stack.Size() - 1;
+            return RemoveReturn(top);
+        }
+        public T Peek()
+        {
+            int top = stack.Size();
+            return stack.Get(top);
+        }
+        public bool Empty()
+        {
+            return stack.IsEmpty();
+        }
+        public int Search(T item)
+        {
+            if (stack.IndexOf(item) == -1) return -1;
+            return stack.IndexOf(item) + 1;
+        }
+        
+    }
+
+
+
+}
