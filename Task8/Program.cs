10354ef28dfe46f2a9718bdabea7ff139a270b8e
OpinDraws <blohin.m.m@gmail.com>
2024-11-07 22:43:50 +0300

    Update Program.cs

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -6,36 +6,6 @@ internal class Program
     {
         MyStack<int> stack = new MyStack<int>();
     }
-    public class MyStack<T> : MyVector<T>
-    {
-        MyVector<T> stack = new MyVector<T>();
-
-        public void Push(T item)
-        {
-            stack.Add(item);
-
-        }
-        public T Pop()
-        {
-            int top = stack.Size() - 1;
-            return RemoveReturn(top);
-        }
-        public T Peek()
-        {
-            int top = stack.Size();
-            return stack.Get(top);
-        }
-        public bool Empty()
-        {
-            return stack.IsEmpty();
-        }
-        public int Search(T item)
-        {
-            if (stack.IndexOf(item) == -1) return -1;
-            return stack.IndexOf(item) + 1;
-        }
-        
-    }
 
 
 
