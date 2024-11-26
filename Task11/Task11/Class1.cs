21f051697d598206b3e95ca532c7adde218662ec
OpinDraws <blohin.m.m@gmail.com>
2024-11-26 23:52:00 +0300

    Add files via upload

---
+++ Task11/Task11/Class1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11
+{
+    public class MyComparer<T> : Comparer<T> where T : IComparable
+    {
+        public override int Compare(T? x, T? y)
+        {
+            return x.CompareTo(y);
+        }
+    }
+    internal class MyHeap<T> where T : IComparable
+    {
+        private T[] array;
+        private int count;
+
+        public MyHeap() { 
+            this.array = new T[0];
+            count = 0;
+        }
+        public MyHeap(int capacity)
+        {
+            this.array = new T[capacity];
+            count = 0;
+        }
+        public MyHeap(T[] array)
+        {
+            if (array == null) { Console.WriteLine("Lol,this empety"); this.array = new T[10]; }
+            else
+            {
+                this.array = (T[])array.Clone();
+                count = array.Length;
+
+            }
+            OrderInHeap();
+        }
+        public int Count { get => count; }
+
+        public void Print()
+        {
+
+            MyHeap<T> help = new MyHeap<T>(array);
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(help.RemoveMax());
+                Console.Write(' ');
+            }
+            Console.WriteLine();
+        }
+
+        private void Swap(int a, int b)
+        {
+            T t = array[a];
+            array[a] = array[b];
+            array[b] = t;
+        }
+        private void Emerge(int indexA)
+        {
+            if (indexA != 0)
+            {
+                int indexB = (indexA - 1) / 2;
+                while (indexA >= 0)
+                {
+
+                    if (indexA.CompareTo(indexB) > 0)
+                    {
+                        Swap(indexA, indexB);
+                        indexA = indexB;
+                        indexB = (indexA + 1) / 2;
+                    }
+                    else break;
+                }
+            }
+        }
+
+        private void Heapify(int valueIndex)
+        {
+            while (true)
+            {
+                int leftChildIndex = valueIndex * 2 + 1;
+                int rightChildIndex = valueIndex * 2 + 2;
+                int largestItemIndex = valueIndex;
+
+                if (leftChildIndex < count && array[leftChildIndex].CompareTo(array[largestItemIndex]) > 0)
+                    largestItemIndex = leftChildIndex;
+
+                if (rightChildIndex < count && array[rightChildIndex].CompareTo(array[largestItemIndex]) > 0)
+                    largestItemIndex = rightChildIndex;
+
+                if (largestItemIndex == valueIndex) break;
+
+                Swap(valueIndex, largestItemIndex);
+
+                valueIndex = largestItemIndex;
+            }
+
+        }
+
+        public void Add(T value)
+        {
+            if (count == array.Length)
+            {
+                ExceptionMemory();
+            }
+            array[count++] = value;
+            Emerge(count - 1);
+        }
+
+        public T RemoveMax()
+        {
+            if (count == 0) throw new IndexOutOfRangeException();
+
+            T removedItem = array[0];
+            if (count > 1)
+            {
+                array[0] = array[count - 1];
+                array[count - 1] = default;
+                if (count > 1) Heapify(0);
+            }
+            else array[0] = default;
+            count--;
+            return removedItem;
+        }
+
+        public T GetMax() => array[0];
+
+        private void OrderAll()
+        {
+            for (int i = count / 2; i >= 0; i--) Heapify(i);
+        }
+
+        public void Add(MyHeap<T> heap)
+        {
+            T[] array = heap.ToArray();
+            foreach (T item in array) Add(item);
+        }
+
+        private void ExceptionMemory()
+        {
+            T[] help = new T[(int)(array.Length * 1.5)];
+            Array.Copy(array, help, array.Length);
+            array = help;
+        }
+
+        public void KeyChange(int index, T val)
+        {
+            array[index] = val;
+            Emerge(index);
+
+        }
+
+        private void OrderInHeap()
+        {
+            for (int i = count / 2; i >= 0; i--) Heapify(i);
+
+        }
+
+        public T[] ToArray()
+        {
+            T[] newItemData = new T[count];
+            Array.Copy(array, newItemData, count);
+            return newItemData;
+        }
+
+        public override string ToString()
+            => $"[{string.Join(", ", ToArray())}]";
+        public IEnumerator GetEnumerator()
+        {
+            return array.GetEnumerator();   
+        }
+        public void Remove(T item)
+        {
+            for(int i=0; i<count; i++)
+           
+                if (array[i].CompareTo(item) > 0) {
+                    array[i] = array[count - 1];
+                    array[count - 1] = default(T);
+                    count--;
+
+                }
+            OrderAll();
+        }
+
+        public void RemoveAll(T[] a)
+        {
+            foreach(T item in a)
+            {
+                Remove(item);
+            }
+            OrderAll();
+        }
+    }
+}
+
---
+++ Task11/Task11/Program.cs
@@ -0,0 +1,142 @@
+﻿using System.Drawing;
+using System.Numerics;
+using Task11;
+
+public class Programm
+{
+    public class MyPriorityQueue<T> where T : IComparable
+    {
+        MyHeap<T> queue;
+        private int size;
+        private IComparer<T> comparer = new MyComparer<T>();
+
+        public int Size() { return size; }
+        public MyPriorityQueue(T[] a)
+        {
+            queue = new MyHeap<T>(a);
+        }
+
+
+        public MyPriorityQueue(int capacity)
+        {
+            queue = new MyHeap<T>(capacity);
+        }
+
+
+        public MyPriorityQueue(int capacity, IComparer<T> comp)
+        {
+            queue = new MyHeap<T>(capacity);
+            size = 0;
+            comparer = comp;
+        }
+
+
+        public MyPriorityQueue(MyPriorityQueue<T> c)
+        {
+            queue = new MyHeap<T>();
+            T[] a = c.ToArray();
+            for (int i = 0; i < a.Length; i++)
+                queue.Add(a[i]);
+            size = queue.Count;
+        }
+
+        public void Add(T e)
+        {
+
+            queue.Add(e);
+            size++;
+        }
+        public void Clear()
+        {
+            queue = new MyHeap<T>();
+            size = 0;
+        }
+        public void AddAll(T[] a)
+        {
+            MyHeap<T> array = new MyHeap<T>(a);
+            queue.Add(array);
+        }
+
+        public T[] ToArray() { return queue.ToArray(); }
+
+        public bool Contains(object o)
+        {
+            T oConverter = (T)o;
+            foreach (T e in queue)
+                if (oConverter.Equals(e))
+                    return true;
+
+            return false;
+        }
+
+        public bool ContainsAll(T[] a)
+        {
+            foreach (T e in a)
+            {
+                bool flag = false;
+                foreach (T t in queue)
+                    if (t.Equals(e) == true)
+                        flag = true;
+                if (!flag) return false;
+            }
+            return true;
+        }
+
+        public void Remove(T e)
+        {
+            queue.Remove(e);
+        }
+        public void RemoveAll(T[] a) {  queue.RemoveAll(a); }
+
+        public bool IsEmpty()
+        {
+            if (size == 0) return true;
+            return false;
+        }
+
+        public void RetainAll(T[] a)
+        {
+            foreach(T el in queue) {
+                bool flag = false; 
+                foreach (T e in a)
+                {
+                    if (el.CompareTo(e) == 0) {flag = true; break;}
+                }
+                if (!flag) Remove(el);
+            }
+        }
+
+        public T Element()
+        {
+            return queue.GetMax();
+        }
+        public T? Peek()
+        {
+            if (size == 0) return default(T);
+            return queue.GetMax();
+        }
+
+        public T Pool()
+        {
+           return queue.RemoveMax();
+        }
+        public void Print()
+        {
+            queue.Print();
+        }
+    }
+    static void Main()
+    {
+        int[] mas = { 4, 3, 1, 7, 5 };
+        char[] mas2 = { 'a', 'b', 'c' };
+        MyPriorityQueue<char> Mpq = new MyPriorityQueue<char>(mas2);
+        MyPriorityQueue<int> Mp = new MyPriorityQueue<int>(mas);
+        Mpq.Print();
+        Console.WriteLine();
+
+
+
+        Mp.Print();
+
+    }
+}
